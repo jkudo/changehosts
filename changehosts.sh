088b6c5e170bc2609cbe3cd791ebecfcaa3cf2b8
jkudo <level69@gmail.com>
2018-11-23 14:17:55 +0900

    fix

--- changehosts.sh
+++ changehosts.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
 BEFORE_HOST=$1
 AFTER_HOST=$2
-HOSTS_FILE=/etc/hosts
 
+HOSTS_FILE=/etc/hosts
 #change host
 sed -i -e "/${AFTER_HOST}/d" $HOSTS_FILE
 arr=(`dig A ${BEFORE_HOST} | grep ^${BEFORE_HOST} | awk '{print $5}'`)
 
 i=0
 for num in ${arr[@]}; do
-   echo "${num} ${AFTER_HOST}" >> ${HOSTS_FILE}
-   let i++
+        echo "${num} ${AFTER_HOST}" >> ${HOSTS_FILE}
+        let i++
 done
